61c0be236a92a7344280828cb65017ccb8124ee5
Kevin Paul <kpaul@ucar.edu>
2015-06-10 12:25:51 -0600

    Updating github and package data

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(name='PyReshaper',
       description='Python Time-Slice to Time-Series NetCDF Converter',
       author='Kevin Paul',
       author_email='kpaul@ucar.edu',
-      url='https://wiki.ucar.edu/display/~kpaul/PyReshaper',
+      url='https://github.com/NCAR-CISL-ASAP/PyReshaper',
       packages=['pyreshaper'],
+      package_data={'pyreshaper': ['LICENSE.txt']},
       scripts=['bin/slice2series'],
       requires=['Nio', 'mpi4py']
      )
