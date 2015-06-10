a7aa517e12c0f2f5ac20c6ff5a86c1c01cab0fbe
Kevin Paul <kpaul@ucar.edu>
2015-06-10 17:06:28 -0600

    Bugfix for PyPI install.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import shutil
-
-# Copy license file into package directory
-shutil.copy('LICENSE.txt', 'source/pyreshaper')
 
 setup(name='PyReshaper',
-      version='0.9.2',
+      version='0.9.3',
       description='Python Time-Slice to Time-Series NetCDF Converter',
       author='Kevin Paul',
       author_email='kpaul@ucar.edu',
       url='https://github.com/NCAR-CISL-ASAP/PyReshaper',
-      download_url='https://github.com/NCAR-CISL-ASAP/PyReshaper/tarball/v0.9.2',
+      download_url='https://github.com/NCAR-CISL-ASAP/PyReshaper/tarball/v0.9.3',
       license='https://github.com/NCAR-CISL-ASAP/PyReshaper/blob/master/LICENSE.txt',
       packages=['pyreshaper'],
       package_dir={'pyreshaper': 'source/pyreshaper'},
