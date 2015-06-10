d6620f1f8824517ffedfeffe6f2b53d6c9615a13
Kevin Paul <kpaul@ucar.edu>
2015-06-10 15:10:20 -0600

    Updating for preparation for PyPI submission

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ import shutil
 shutil.copy('LICENSE.txt', 'src/pyreshaper')
 
 setup(name='PyReshaper',
-      version='0.9.1',
+      version='0.9.2',
       description='Python Time-Slice to Time-Series NetCDF Converter',
       author='Kevin Paul',
       author_email='kpaul@ucar.edu',
       url='https://github.com/NCAR-CISL-ASAP/PyReshaper',
+      download_url='https://github.com/NCAR-CISL-ASAP/PyReshaper/tarball/v0.9.2',
       license='https://github.com/NCAR-CISL-ASAP/PyReshaper/blob/master/LICENSE.txt',
       packages=['pyreshaper'],
       package_dir={'pyreshaper': 'src/pyreshaper'},
