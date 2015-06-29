163a2daf60b097bfbf3bb291f0e1ca882c2d939f
Kevin Paul <kpaul@ucar.edu>
2015-06-29 12:27:20 -0600

    Adding asaptools as a dependency

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ Copyright 2015, University Corporation for Atmospheric Research
 See the LICENSE.txt file for details
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='PyReshaper',
-      version='0.9.3',
+      version='0.9.4',
       description='Python Time-Slice to Time-Series NetCDF Converter',
       author='Kevin Paul',
       author_email='kpaul@ucar.edu',
       url='https://github.com/NCAR-CISL-ASAP/PyReshaper',
-      download_url='https://github.com/NCAR-CISL-ASAP/PyReshaper/tarball/v0.9.3',
+      download_url='https://github.com/NCAR-CISL-ASAP/PyReshaper/tarball/v0.9.4',
       license='https://github.com/NCAR-CISL-ASAP/PyReshaper/blob/master/LICENSE.txt',
       packages=['pyreshaper'],
       package_dir={'pyreshaper': 'source/pyreshaper'},
       package_data={'pyreshaper': ['LICENSE.txt']},
       scripts=['bin/slice2series'],
-      requires=['Nio', 'mpi4py']
+      install_requires=['Nio', 'mpi4py>=1.3', 'asaptools>=0.4']
       )
