f98334937f536ba212035e490617565573b40c6f
Kevin Paul <kpaul@ucar.edu>
2015-07-06 15:42:44 -0600

    Creating stub to hold common testing routines

---
+++ tests/yellowstone/utilities/testtools.py
@@ -0,0 +1,8 @@
+#==============================================================================
+#
+#  TestTools
+#
+#  This is a collection of functions that are useful for running the PyReshaper
+#  tests on the Yellowstone compute system.
+#
+#==============================================================================
