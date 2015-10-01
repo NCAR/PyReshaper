8f5bc1a30847575578b03753deb9ef95ab8dcc4a
Kevin Paul <kpaul@ucar.edu>
2015-10-01 13:35:43 -0600

    Remove debugging output

--- tests/yellowstone/checkresults.py
+++ tests/yellowstone/checkresults.py
@@ -322,8 +322,6 @@ if __name__ == '__main__':
         for i in range(comm.get_size() - 1):
             results.extend(comm.collect()[1])
 
-        print results
-
         for test_name, test_info in tests_to_check.items():
             summfile = os.path.join(test_info['run'], test_name + '.cprnc')
             sfile = open(summfile, 'w')
