a6c46d4c02b2f7f95b9fb16f3c28d7c897964030
Kevin Paul <kpaul@ucar.edu>
2015-10-07 11:05:20 -0600

    Adding scipts to install

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(name='PyReshaper',
       packages=['pyreshaper'],
       package_dir={'pyreshaper': 'source/pyreshaper'},
       package_data={'pyreshaper': ['LICENSE.rst']},
-      scripts=['bin/slice2series'],
+      scripts=['bin/makespec', 'bin/runspec'],
       install_requires=['mpi4py>=1.3', 'asaptools>=0.4']
       )
