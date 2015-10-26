753d3fcf3a300c3f45e72c8eb28c9372110f8193
Kevin Paul <kpaul@ucar.edu>
2015-10-26 14:09:14 -0600

    Finished tests of s2smake script

--- source/pyreshaper/test/s2smakeTests.py
+++ source/pyreshaper/test/s2smakeTests.py
@@ -3,50 +3,26 @@ Copyright 2015, University Corporation for Atmospheric Research
 See LICENSE.txt for details
 """
 
+import os
 import imp
 import sys
 import unittest
+import cPickle as pickle
 
-from StringIO import StringIO
+from pyreshaper.specification import Specifier
 
 s2smake = imp.load_source('s2smake', '../../../scripts/s2smake')
 
 
 class s2smakeTest(unittest.TestCase):
 
-    def setUp(self):
-        self.saved_stdout = sys.stdout
-        self.saved_stderr = sys.stderr
-        self.stdout = StringIO()
-        self.stderr = StringIO()
-        sys.stdout = self.stdout
-        sys.stderr = self.stderr
-
-    def tearDown(self):
-        sys.stdout = self.saved_stdout
-        sys.stderr = self.saved_stderr
-        print '_' * 7
-        print 'STDOUT:'
-        print '-' * 7
-        print self.outstr
-        print '_' * 7
-        print 'STDERR:'
-        print '-' * 7
-        print self.errstr
-
     def test_CLI_empty(self):
         argv = []
         self.assertRaises(SystemExit, s2smake.cli, argv)
-        self.outstr = self.stdout.getvalue().strip()
-        self.assertEqual(self.outstr, '', 'STDOUT Unexpected')
-        self.errstr = self.stderr.getvalue().strip()
 
     def test_CLI_help(self):
         argv = ['-h']
         self.assertRaises(SystemExit, s2smake.cli, argv)
-        self.outstr = self.stdout.getvalue().strip()
-        self.errstr = self.stderr.getvalue().strip()
-        self.assertEqual(self.errstr, '', 'STDERR Unexpected')
 
     def test_CLI_defaults(self):
         argv = ['s2smakeTests.py']
@@ -65,16 +41,12 @@ class s2smakeTest(unittest.TestCase):
                          'Default output suffix is not ".nc"')
         self.assertEqual(args.specfile, 'input.s2s',
                          'Default output suffix is not ".nc"')
-        self.outstr = self.stdout.getvalue().strip()
-        self.assertEqual(self.outstr, '', 'STDOUT Unexpected')
-        self.errstr = self.stderr.getvalue().strip()
-        self.assertEqual(self.errstr, '', 'STDERR Unexpected')
 
     def test_CLI_set_all_short(self):
         clevel = 3
         ncfmt = 'netcdf'
         metadata = ['meta1', 'meta2']
-        specfile = 'mysepc.s2s'
+        specfile = 'myspec.s2s'
         prefix = 'prefix.'
         suffix = '.suffix'
         infiles = ['s2smakeTests.py', 'specificationTests.py']
@@ -100,16 +72,12 @@ class s2smakeTest(unittest.TestCase):
                          'Default output suffix is not {!r}'.format(suffix))
         self.assertEqual(args.specfile, specfile,
                          'Default output suffix is not {!r}'.format(specfile))
-        self.outstr = self.stdout.getvalue().strip()
-        self.assertEqual(self.outstr, '', 'STDOUT Unexpected')
-        self.errstr = self.stderr.getvalue().strip()
-        self.assertEqual(self.errstr, '', 'STDERR Unexpected')
 
     def test_CLI_set_all_long(self):
         clevel = 3
         ncfmt = 'netcdf'
         metadata = ['meta1', 'meta2']
-        specfile = 'mysepc.s2s'
+        specfile = 'myspec.s2s'
         prefix = 'prefix.'
         suffix = '.suffix'
         infiles = ['s2smakeTests.py', 'specificationTests.py']
@@ -136,10 +104,107 @@ class s2smakeTest(unittest.TestCase):
                          'Default output suffix is not {!r}'.format(suffix))
         self.assertEqual(args.specfile, specfile,
                          'Default output suffix is not {!r}'.format(specfile))
-        self.outstr = self.stdout.getvalue().strip()
-        self.assertEqual(self.outstr, '', 'STDOUT Unexpected')
-        self.errstr = self.stderr.getvalue().strip()
-        self.assertEqual(self.errstr, '', 'STDERR Unexpected')
+
+    def test_main_defaults(self):
+        argv = ['s2smakeTests.py']
+        specfile = 'input.s2s'
+        if os.path.exists(specfile):
+            os.remove(specfile)
+        s2smake.main(argv)
+        self.assertTrue(os.path.exists(specfile), 'Specfile not found')
+        spec = pickle.load(open(specfile, 'rb'))
+        os.remove(specfile)
+        self.assertTrue(isinstance(spec, Specifier),
+                        'Specfile does not contain a Specifier')
+        self.assertEqual(spec.compression_level, 1,
+                         'Default compression level is not 1')
+        self.assertListEqual(spec.input_file_list, argv,
+                             'Default infiles list is not {}'.format(argv))
+        self.assertListEqual(spec.time_variant_metadata, [],
+                             'Default metadata list is not []')
+        self.assertEqual(spec.netcdf_format, 'netcdf4',
+                         'Default NetCDF format is not "netcdf4"')
+        self.assertEqual(spec.output_file_prefix, os.path.abspath('tseries.'),
+                         'Default output prefix is not "tseries."')
+        self.assertEqual(spec.output_file_suffix, '.nc',
+                         'Default output suffix is not ".nc"')
+
+    def test_main_set_all_short(self):
+        clevel = 3
+        ncfmt = 'netcdf'
+        metadata = ['meta1', 'meta2']
+        specfile = 'myspec.s2s'
+        prefix = 'prefix.'
+        suffix = '.suffix'
+        infiles = ['s2smakeTests.py', 'specificationTests.py']
+
+        argv = ['-c', str(clevel), '-f', ncfmt]
+        for md in metadata:
+            argv.extend(['-m', md])
+        argv.extend(['-o', specfile, '-p', prefix, '-s', suffix])
+        argv.extend(infiles)
+
+        if os.path.exists(specfile):
+            os.remove(specfile)
+        s2smake.main(argv)
+
+        self.assertTrue(os.path.exists(specfile), 'Specfile not found')
+        spec = pickle.load(open(specfile, 'rb'))
+        os.remove(specfile)
+
+        self.assertTrue(isinstance(spec, Specifier),
+                        'Specfile does not contain a Specifier')
+        self.assertEqual(spec.compression_level, clevel,
+                         'Default compression level is not {!r}'.format(clevel))
+        self.assertListEqual(spec.input_file_list, infiles,
+                             'Default infiles list is not {}'.format(infiles))
+        self.assertListEqual(spec.time_variant_metadata, metadata,
+                             'Default metadata list is not {}'.format(metadata))
+        self.assertEqual(spec.netcdf_format, ncfmt,
+                         'Default NetCDF format is not {!r}'.format(ncfmt))
+        self.assertEqual(spec.output_file_prefix, os.path.abspath(prefix),
+                         'Default output prefix is not {!r}'.format(prefix))
+        self.assertEqual(spec.output_file_suffix, suffix + '.nc',
+                         'Default output suffix is not {!r}'.format(suffix))
+
+    def test_main_set_all_long(self):
+        clevel = 3
+        ncfmt = 'netcdf'
+        metadata = ['meta1', 'meta2']
+        specfile = 'myspec.s2s'
+        prefix = 'prefix.'
+        suffix = '.suffix'
+        infiles = ['s2smakeTests.py', 'specificationTests.py']
+
+        argv = ['--compression_level', str(clevel), '--netcdf_format', ncfmt]
+        for md in metadata:
+            argv.extend(['--metadata', md])
+        argv.extend(['--specfile', specfile, '--output_prefix', prefix,
+                     '--output_suffix', suffix])
+        argv.extend(infiles)
+
+        if os.path.exists(specfile):
+            os.remove(specfile)
+        s2smake.main(argv)
+
+        self.assertTrue(os.path.exists(specfile), 'Specfile not found')
+        spec = pickle.load(open(specfile, 'rb'))
+        os.remove(specfile)
+
+        self.assertTrue(isinstance(spec, Specifier),
+                        'Specfile does not contain a Specifier')
+        self.assertEqual(spec.compression_level, clevel,
+                         'Default compression level is not {!r}'.format(clevel))
+        self.assertListEqual(spec.input_file_list, infiles,
+                             'Default infiles list is not {}'.format(infiles))
+        self.assertListEqual(spec.time_variant_metadata, metadata,
+                             'Default metadata list is not {}'.format(metadata))
+        self.assertEqual(spec.netcdf_format, ncfmt,
+                         'Default NetCDF format is not {!r}'.format(ncfmt))
+        self.assertEqual(spec.output_file_prefix, os.path.abspath(prefix),
+                         'Default output prefix is not {!r}'.format(prefix))
+        self.assertEqual(spec.output_file_suffix, suffix + '.nc',
+                         'Default output suffix is not {!r}'.format(suffix))
 
 if __name__ == "__main__":
     unittest.main()
