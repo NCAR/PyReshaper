4b0bd8fe4382143c3e4f755c02a3c6f74470b261
Kevin Paul <kpaul@ucar.edu>
2016-03-05 16:52:12 -0700

    Backporting s2smake to Python 2.6

--- source/pyreshaper/test/s2smakeTests.py
+++ source/pyreshaper/test/s2smakeTests.py
@@ -17,7 +17,7 @@ class s2smakeTest(unittest.TestCase):
 
     def test_CLI_empty(self):
         argv = []
-        self.assertRaises(SystemExit, s2smake.cli, argv)
+        self.assertRaises(ValueError, s2smake.cli, argv)
 
     def test_CLI_help(self):
         argv = ['-h']
@@ -25,20 +25,21 @@ class s2smakeTest(unittest.TestCase):
 
     def test_CLI_defaults(self):
         argv = ['s2smakeTests.py']
-        args = s2smake.cli(argv)
-        self.assertEqual(args.compression_level, 1,
+        opts, args = s2smake.cli(argv)
+        self.assertEqual(opts.compression_level, 1,
                          'Default compression level is not 1')
-        self.assertListEqual(args.infiles, argv,
-                             'Default infiles list is not {}'.format(argv))
-        self.assertListEqual(args.metadata, [],
-                             'Default metadata list is not []')
-        self.assertEqual(args.netcdf_format, 'netcdf4',
+        for i1, i2 in zip(args, argv):
+            self.assertEqual(i1, i2,
+                             'Default infiles list is not {0}'.format(argv))
+        self.assertEqual(len(opts.metadata), 0,
+                         'Default metadata list is not []')
+        self.assertEqual(opts.netcdf_format, 'netcdf4',
                          'Default NetCDF format is not "netcdf4"')
-        self.assertEqual(args.output_prefix, 'tseries.',
+        self.assertEqual(opts.output_prefix, 'tseries.',
                          'Default output prefix is not "tseries."')
-        self.assertEqual(args.output_suffix, '.nc',
+        self.assertEqual(opts.output_suffix, '.nc',
                          'Default output suffix is not ".nc"')
-        self.assertEqual(args.specfile, 'input.s2s',
+        self.assertEqual(opts.specfile, 'input.s2s',
                          'Default output suffix is not ".nc"')
 
     def test_CLI_set_all_short(self):
@@ -55,22 +56,29 @@ class s2smakeTest(unittest.TestCase):
             argv.extend(['-m', md])
         argv.extend(['-o', specfile, '-p', prefix, '-s', suffix])
         argv.extend(infiles)
-        args = s2smake.cli(argv)
+        opts, args = s2smake.cli(argv)
+        print opts.metadata
 
-        self.assertEqual(args.compression_level, clevel,
-                         'Default compression level is not {!r}'.format(clevel))
-        self.assertListEqual(args.infiles, infiles,
-                             'Default infiles list is not {}'.format(infiles))
-        self.assertListEqual(args.metadata, metadata,
-                             'Default metadata list is not {}'.format(metadata))
-        self.assertEqual(args.netcdf_format, ncfmt,
-                         'Default NetCDF format is not {!r}'.format(ncfmt))
-        self.assertEqual(args.output_prefix, prefix,
-                         'Default output prefix is not {!r}'.format(prefix))
-        self.assertEqual(args.output_suffix, suffix,
-                         'Default output suffix is not {!r}'.format(suffix))
-        self.assertEqual(args.specfile, specfile,
-                         'Default output suffix is not {!r}'.format(specfile))
+        self.assertEqual(opts.compression_level, clevel,
+                         'Default compression level is not {0!r}'.format(clevel))
+        self.assertEqual(len(args), len(infiles),
+                         'Default infiles is not of length {0}'.format(len(infiles)))
+        for i1, i2 in zip(args, infiles):
+            self.assertEqual(i1, i2,
+                             'Default infiles list is not {0}'.format(infiles))
+        self.assertEqual(len(opts.metadata), len(metadata),
+                         'Default metadata list is not of length {0}'.format(len(metadata)))
+        for i1, i2 in zip(opts.metadata, metadata):
+            self.assertEqual(i1, i2,
+                             'Default metadata list is not {0}'.format(metadata))
+        self.assertEqual(opts.netcdf_format, ncfmt,
+                         'Default NetCDF format is not {0!r}'.format(ncfmt))
+        self.assertEqual(opts.output_prefix, prefix,
+                         'Default output prefix is not {0!r}'.format(prefix))
+        self.assertEqual(opts.output_suffix, suffix,
+                         'Default output suffix is not {0!r}'.format(suffix))
+        self.assertEqual(opts.specfile, specfile,
+                         'Default output suffix is not {0!r}'.format(specfile))
 
     def test_CLI_set_all_long(self):
         clevel = 3
@@ -87,22 +95,26 @@ class s2smakeTest(unittest.TestCase):
         argv.extend(['--specfile', specfile, '--output_prefix', prefix,
                      '--output_suffix', suffix])
         argv.extend(infiles)
-        args = s2smake.cli(argv)
+        opts, args = s2smake.cli(argv)
 
-        self.assertEqual(args.compression_level, clevel,
-                         'Default compression level is not {!r}'.format(clevel))
-        self.assertListEqual(args.infiles, infiles,
-                             'Default infiles list is not {}'.format(infiles))
-        self.assertListEqual(args.metadata, metadata,
-                             'Default metadata list is not {}'.format(metadata))
-        self.assertEqual(args.netcdf_format, ncfmt,
-                         'Default NetCDF format is not {!r}'.format(ncfmt))
-        self.assertEqual(args.output_prefix, prefix,
-                         'Default output prefix is not {!r}'.format(prefix))
-        self.assertEqual(args.output_suffix, suffix,
-                         'Default output suffix is not {!r}'.format(suffix))
-        self.assertEqual(args.specfile, specfile,
-                         'Default output suffix is not {!r}'.format(specfile))
+        self.assertEqual(opts.compression_level, clevel,
+                         'Default compression level is not {0!r}'.format(clevel))
+        self.assertEqual(len(args), len(infiles),
+                         'Default infiles list is not of length {0}'.format(len(infiles)))
+        for i1, i2 in zip(args, infiles):
+            self.assertEqual(i1, i2,
+                             'Default infiles has no {0}'.format(infiles))
+        for i1, i2 in zip(opts.metadata, metadata):
+            self.assertEqual(i1, i2,
+                             'Default metadata list is not {0}'.format(metadata))
+        self.assertEqual(opts.netcdf_format, ncfmt,
+                         'Default NetCDF format is not {0!r}'.format(ncfmt))
+        self.assertEqual(opts.output_prefix, prefix,
+                         'Default output prefix is not {0!r}'.format(prefix))
+        self.assertEqual(opts.output_suffix, suffix,
+                         'Default output suffix is not {0!r}'.format(suffix))
+        self.assertEqual(opts.specfile, specfile,
+                         'Default output suffix is not {0!r}'.format(specfile))
 
     def test_main_defaults(self):
         argv = ['s2smakeTests.py']
@@ -117,10 +129,13 @@ class s2smakeTest(unittest.TestCase):
                         'Specfile does not contain a Specifier')
         self.assertEqual(spec.compression_level, 1,
                          'Default compression level is not 1')
-        self.assertListEqual(spec.input_file_list, argv,
-                             'Default infiles list is not {}'.format(argv))
-        self.assertListEqual(spec.time_variant_metadata, [],
-                             'Default metadata list is not []')
+        self.assertEqual(len(spec.input_file_list), len(argv),
+                        'Default infiles is not of lenght {0}'.format(len(argv)))
+        for i1, i2 in zip(spec.input_file_list, argv):
+            self.assertEqual(i1, i2,
+                             'Default infiles list is not {0}'.format(argv))
+        self.assertEqual(len(spec.time_variant_metadata), 0,
+                         'Default metadata list is not of length 0')
         self.assertEqual(spec.netcdf_format, 'netcdf4',
                          'Default NetCDF format is not "netcdf4"')
         self.assertEqual(spec.output_file_prefix, os.path.abspath('tseries.'),
@@ -154,17 +169,23 @@ class s2smakeTest(unittest.TestCase):
         self.assertTrue(isinstance(spec, Specifier),
                         'Specfile does not contain a Specifier')
         self.assertEqual(spec.compression_level, clevel,
-                         'Default compression level is not {!r}'.format(clevel))
-        self.assertListEqual(spec.input_file_list, infiles,
-                             'Default infiles list is not {}'.format(infiles))
-        self.assertListEqual(spec.time_variant_metadata, metadata,
-                             'Default metadata list is not {}'.format(metadata))
+                         'Default compression level is not {0!r}'.format(clevel))
+        self.assertEqual(len(spec.input_file_list), len(infiles),
+                        'Default infiles is not of lenght {0}'.format(len(infiles)))
+        for i1, i2 in zip(spec.input_file_list, infiles):
+            self.assertEqual(i1, i2,
+                             'Default infiles list is not {0}'.format(argv))
+        self.assertEqual(len(spec.time_variant_metadata), len(metadata),
+                         'Default metadata list is not of length {0}'.format(len(metadata)))
+        for i1, i2 in zip(spec.time_variant_metadata, metadata):
+            self.assertEqual(i1, i2,
+                             'Default metadata list is not {0}'.format(metadata))
         self.assertEqual(spec.netcdf_format, ncfmt,
-                         'Default NetCDF format is not {!r}'.format(ncfmt))
+                         'Default NetCDF format is not {0!r}'.format(ncfmt))
         self.assertEqual(spec.output_file_prefix, os.path.abspath(prefix),
-                         'Default output prefix is not {!r}'.format(prefix))
+                         'Default output prefix is not {0!r}'.format(prefix))
         self.assertEqual(spec.output_file_suffix, suffix + '.nc',
-                         'Default output suffix is not {!r}'.format(suffix))
+                         'Default output suffix is not {0!r}'.format(suffix))
 
     def test_main_set_all_long(self):
         clevel = 3
@@ -193,17 +214,23 @@ class s2smakeTest(unittest.TestCase):
         self.assertTrue(isinstance(spec, Specifier),
                         'Specfile does not contain a Specifier')
         self.assertEqual(spec.compression_level, clevel,
-                         'Default compression level is not {!r}'.format(clevel))
-        self.assertListEqual(spec.input_file_list, infiles,
-                             'Default infiles list is not {}'.format(infiles))
-        self.assertListEqual(spec.time_variant_metadata, metadata,
-                             'Default metadata list is not {}'.format(metadata))
+                         'Default compression level is not {0!r}'.format(clevel))
+        self.assertEqual(len(spec.input_file_list), len(infiles),
+                        'Default infiles is not of lenght {0}'.format(len(infiles)))
+        for i1, i2 in zip(spec.input_file_list, infiles):
+            self.assertEqual(i1, i2,
+                             'Default infiles list is not {0}'.format(argv))
+        self.assertEqual(len(spec.time_variant_metadata), len(metadata),
+                         'Default metadata list is not of length {0}'.format(len(metadata)))
+        for i1, i2 in zip(spec.time_variant_metadata, metadata):
+            self.assertEqual(i1, i2,
+                             'Default metadata list is not {0}'.format(metadata))
         self.assertEqual(spec.netcdf_format, ncfmt,
-                         'Default NetCDF format is not {!r}'.format(ncfmt))
+                         'Default NetCDF format is not {0!r}'.format(ncfmt))
         self.assertEqual(spec.output_file_prefix, os.path.abspath(prefix),
-                         'Default output prefix is not {!r}'.format(prefix))
+                         'Default output prefix is not {0!r}'.format(prefix))
         self.assertEqual(spec.output_file_suffix, suffix + '.nc',
-                         'Default output suffix is not {!r}'.format(suffix))
+                         'Default output suffix is not {0!r}'.format(suffix))
 
 if __name__ == "__main__":
     unittest.main()
