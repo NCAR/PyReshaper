e71a2977f0e4298d90931a879d6266a0e60c7135
Kevin Paul <kpaul@ucar.edu>
2019-10-02 17:34:53 -0600

    Adding circleci configuration

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,6 +4,6 @@ set -e
 set -eo pipefail
 
 source activate ${ENV_NAME}
-pytest -n 2 --junitxml=test-reports/junit.xml --cov=./ --verbose
+pytest --junitxml=test-reports/junit.xml --cov=./ --verbose
 echo "[Upload coverage]"
 codecov
