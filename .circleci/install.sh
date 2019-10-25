089b12c091f89167d66a12c0653dd7ace0ac887f
Kevin Paul <kpaul@ucar.edu>
2019-10-24 21:07:29 -0600

    Attempting CI again...

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda config --add channels conda-forge
-conda env create -f .circlci/environment-${PYTHON}.yml --name=${ENV_NAME} --quiet
+conda env create --file .circleci/env-${PYTHON}.yml --name ${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
 pip install pip --upgrade
