bd081b8e170cd9eac7014d13f50284ffacc12430
Kevin Paul <kpaul@ucar.edu>
2019-11-20 10:12:44 -0700

    Remove unused import

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 __version__ = '1.0.6'
 
