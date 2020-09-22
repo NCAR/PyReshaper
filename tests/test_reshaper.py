6aa5a34cee1c59e14567061a28e228da2e3502d1
Kevin Paul <kpaul@ucar.edu>
2020-09-22 14:46:13 -0600

    Remove mention of MPI

--- tests/test_reshaper.py
+++ tests/test_reshaper.py
@@ -12,22 +12,14 @@ from glob import glob
 from os import linesep as eol, remove
 from os.path import exists
 
-from mpi4py import MPI
-
 import mkTestData
 from pyreshaper.reshaper import Reshaper, create_reshaper
 from pyreshaper.specification import Specifier
-
-MPI_COMM_WORLD = MPI.COMM_WORLD  # @UndefinedVariable
 
 
 class CommonTestsBase(object):
 
     def setUp(self):
-
-        # Parallel Management - Just for Tests
-        self.rank = MPI_COMM_WORLD.Get_rank()
-        self.size = MPI_COMM_WORLD.Get_size()
 
         # Default arguments for testing
         self.spec_args = {'infiles': mkTestData.slices,
@@ -50,32 +42,27 @@ class CommonTestsBase(object):
 
         # Test Data Generation
         self.clean()
-        if self.rank == 0:
-            mkTestData.generate_data()
-        MPI_COMM_WORLD.Barrier()
+        mkTestData.generate_data()
 
     def clean(self):
-        if self.rank == 0:
-            for ncfile in glob('*.nc'):
-                remove(ncfile)
-        MPI_COMM_WORLD.Barrier()
+        for ncfile in glob('*.nc'):
+            remove(ncfile)
 
     def header(self):
-        if self.rank == 0:
-            mf = len(mkTestData.slices)
-            mt = len(mkTestData.tsvars)
-            nf = len(self.spec_args['infiles'])
-            nt = mt if self.spec_args['timeseries'] is None else len(
-                self.spec_args['timeseries'])
+        mf = len(mkTestData.slices)
+        mt = len(mkTestData.tsvars)
+        nf = len(self.spec_args['infiles'])
+        nt = mt if self.spec_args['timeseries'] is None else len(
+            self.spec_args['timeseries'])
 
-            hline = '-' * 100
-            hdrstr = ['', hline, '{}.{}:'.format(self.__class__.__name__, inspect.stack()[1][3]), '',
-                      '   specifier({}/{} infile(s), {}/{} TSV(s), ncfmt={ncfmt}, compression={compression}, meta1d={meta1d}, backend={backend}, metafile={metafile})'.format(
-                          nf, mf, nt, mt, **self.spec_args),
-                      '   create(serial={serial}, verbosity={verbosity}, wmode={wmode}, once={once}, simplecomm={simplecomm})'.format(
-                          **self.create_args),
-                      '   convert(output_limit={output_limit}, rchunks={rchunks}, wchunks={wchunks})'.format(**self.convert_args), hline, '']
-            print(eol.join(hdrstr))
+        hline = '-' * 100
+        hdrstr = ['', hline, '{}.{}:'.format(self.__class__.__name__, inspect.stack()[1][3]), '',
+                  '   specifier({}/{} infile(s), {}/{} TSV(s), ncfmt={ncfmt}, compression={compression}, meta1d={meta1d}, backend={backend}, metafile={metafile})'.format(
+                      nf, mf, nt, mt, **self.spec_args),
+                  '   create(serial={serial}, verbosity={verbosity}, wmode={wmode}, once={once}, simplecomm={simplecomm})'.format(
+                      **self.create_args),
+                  '   convert(output_limit={output_limit}, rchunks={rchunks}, wchunks={wchunks})'.format(**self.convert_args), hline, '']
+        print(eol.join(hdrstr))
 
     def check(self, tsvar):
         args = {}
@@ -91,163 +78,133 @@ class CommonTestsBase(object):
         assert len(failed_assertions) == 0, eol.join(assert_msgs)
 
     def convert(self, print_diags=False):
-        if not (self.create_args.get('serial', False) and self.rank > 0):
-            if self.create_args.get('verbosity', 1) == 0:
-                oldout = sys.stdout
-                newout = StringIO()
-                sys.stdout = newout
+        if self.create_args.get('verbosity', 1) == 0:
+            oldout = sys.stdout
+            newout = StringIO()
+            sys.stdout = newout
 
-            spec = Specifier(**self.spec_args)
-            rshpr = create_reshaper(spec, **self.create_args)
-            assert type(rshpr) == Reshaper, 'type(reshaper) failure'
-            rshpr.convert(**self.convert_args)
+        spec = Specifier(**self.spec_args)
+        rshpr = create_reshaper(spec, **self.create_args)
+        assert type(rshpr) == Reshaper, 'type(reshaper) failure'
+        rshpr.convert(**self.convert_args)
 
-            if self.create_args.get('verbosity', 1) == 0:
-                actual = newout.getvalue()
-                assert actual == '', 'stdout should be empty'
-                sys.stdout = oldout
+        if self.create_args.get('verbosity', 1) == 0:
+            actual = newout.getvalue()
+            assert actual == '', 'stdout should be empty'
+            sys.stdout = oldout
 
-            if print_diags:
-                rshpr.print_diagnostics()
-        MPI_COMM_WORLD.Barrier()
+        if print_diags:
+            rshpr.print_diagnostics()
 
     def test_defaults(self):
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_I1(self):
         self.spec_args['infiles'] = mkTestData.slices[1:2]
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_V0(self):
         self.create_args['verbosity'] = 0
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_V3(self):
         self.create_args['verbosity'] = 3
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_TSV2(self):
         self.spec_args['timeseries'] = mkTestData.tsvars[1:3] + ['tsvarX']
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in self.spec_args['timeseries']:
-                if tsvar in mkTestData.tsvars:
-                    self.check(tsvar)
-                else:
-                    fname = self.spec_args['prefix'] + \
-                        tsvar + self.spec_args['suffix']
-                    assert not exists(fname), 'File {0!r} should not exist'.format(fname)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in self.spec_args['timeseries']:
+            if tsvar in mkTestData.tsvars:
+                self.check(tsvar)
+            else:
+                fname = self.spec_args['prefix'] + \
+                    tsvar + self.spec_args['suffix']
+                assert not exists(fname), 'File {0!r} should not exist'.format(fname)
 
     def test_exclude(self):
         self.spec_args['exclude_list'] = mkTestData.timvars[0:1]
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                fname = (self.spec_args['prefix'] + tsvar + self.spec_args['suffix'])
-                for timvar in mkTestData.timvars:
-                    if timvar in self.spec_args['exclude_list']:
-                        xassert = self.assertFalse
-                    else:
-                        xassert = self.assertTrue
-                    xassert(mkTestData.check_var_in(timvar, fname))
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            fname = (self.spec_args['prefix'] + tsvar + self.spec_args['suffix'])
+            for timvar in mkTestData.timvars:
+                if timvar in self.spec_args['exclude_list']:
+                    xassert = self.assertFalse
+                else:
+                    xassert = self.assertTrue
+                xassert(mkTestData.check_var_in(timvar, fname))
 
     def test_NC3(self):
         self.spec_args['ncfmt'] = 'netcdf'
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_ser(self):
         self.create_args['serial'] = True
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_CL1(self):
         self.spec_args['compression'] = 1
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_read_chunking(self):
         self.convert_args['rchunks'] = {'lat': 1, 'time': mkTestData.ntime}
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_write_chunking(self):
         self.convert_args['wchunks'] = {'lat': 1, 'time': mkTestData.ntime}
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_meta1d(self):
         self.spec_args['meta1d'] = True
         self.spec_args['metadata'] = [v for v in mkTestData.tvmvars]
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_metafile(self):
         self.spec_args['metafile'] = 'metafile.nc'
         self.spec_args['metadata'] = [v for v in mkTestData.tvmvars]
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_once(self):
         self.create_args['once'] = True
         self.header()
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_overwrite(self):
         self.create_args['wmode'] = 'o'
@@ -256,10 +213,8 @@ class CommonTestsBase(object):
         self.convert()
         self.create_args['verbosity'] = 1
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_skip(self):
         self.create_args['wmode'] = 's'
@@ -268,10 +223,8 @@ class CommonTestsBase(object):
         self.convert()
         self.create_args['verbosity'] = 1
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_append(self):
         self.create_args['wmode'] = 'a'
@@ -282,10 +235,8 @@ class CommonTestsBase(object):
         self.create_args['wmode'] = 'a'
         self.spec_args['infiles'] = mkTestData.slices[2:]
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            self.check(tsvar)
 
     def test_append_missing(self):
         missing = mkTestData.tsvars[2]
@@ -294,20 +245,17 @@ class CommonTestsBase(object):
         self.create_args['wmode'] = 'w'
         self.spec_args['infiles'] = mkTestData.slices[0:2]
         self.convert()
-        if self.rank == 0:
-            remove(self.spec_args['prefix'] + missing + self.spec_args['suffix'])
-        MPI_COMM_WORLD.Barrier()
+        remove(self.spec_args['prefix'] + missing + self.spec_args['suffix'])
+
         self.create_args['wmode'] = 'a'
         self.spec_args['infiles'] = mkTestData.slices[2:]
         self.convert()
-        if self.rank == 0:
-            for tsvar in mkTestData.tsvars:
-                if tsvar == missing:
-                    self.spec_args['infiles'] = mkTestData.slices[2:]
-                else:
-                    self.spec_args['infiles'] = mkTestData.slices
-                self.check(tsvar)
-        MPI_COMM_WORLD.Barrier()
+        for tsvar in mkTestData.tsvars:
+            if tsvar == missing:
+                self.spec_args['infiles'] = mkTestData.slices[2:]
+            else:
+                self.spec_args['infiles'] = mkTestData.slices
+            self.check(tsvar)
 
 
 class NetCDF4Tests(unittest.TestCase, CommonTestsBase):
