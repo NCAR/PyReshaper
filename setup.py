5d30ed7233115741d3a0a734f32fc2d3b89df847
Kevin Paul <kpaul@ucar.edu>
2020-09-23 11:25:09 -0600

    bumpversion minor

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ limitations under the License.
 
 from setuptools import find_packages, setup
 
-__version__ = '1.0.7'
+__version__ = '1.1.0'
 
 with open('README.rst', 'r') as fh:
     long_description = fh.read()
